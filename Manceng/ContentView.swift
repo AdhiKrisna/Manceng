049cf756857edfb993a8c3c9157235e3ef3eddd0
Made Vidyatma Adhi Krisna <krisnahmbtn@gmail.com>
2026-06-10 12:02:21 +0700

    Initial Commit

---
+++ Manceng/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Manceng
+//
+//  Created by Made Vidyatma Adhi Krisna on 10/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Manceng/MancengApp.swift
@@ -0,0 +1,17 @@
+//
+//  MancengApp.swift
+//  Manceng
+//
+//  Created by Made Vidyatma Adhi Krisna on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MancengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
